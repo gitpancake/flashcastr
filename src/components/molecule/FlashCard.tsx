30ab6669c17335c72c5b3a3729934e4487908b3a
henry <13052648+gitpancake@users.noreply.github.com>
2025-05-04 21:52:47 -0700

    Fix up spacing

--- src/components/molecule/FlashCard.tsx
+++ src/components/molecule/FlashCard.tsx
@@ -13,13 +13,13 @@ const AVATAR_PLACEHOLDER = "/splash.png"; // Adjust path as needed
 
 export default function FlashCard({ player, city, timeAgo, flashNumber, imageUrl, ref }: FlashCardProps) {
   return (
-    <div className="bg-[#1E1E1E] p-2 flex items-center justify-between w-full max-w-xl max-h-[80px]" ref={ref}>
+    <div className="bg-[#1E1E1E] p-2 flex items-center justify-between w-full max-w-xl max-h-[100px]" ref={ref}>
       <div className="flex items-start gap-3">
         <img src={AVATAR_PLACEHOLDER} alt="avatar" className="w-[30px] h-[30px] object-cover shadow-lg" />
 
-        <div className="flex flex-col gap-2 h-full">
+        <div className="flex flex-col gap-3 h-full">
           <div className="flex flex-col gap-0">
-            <p className="font-invader text-white text-[36px] leading-none">{player}</p>
+            <p className="font-invader text-white text-[28px] leading-none">{player}</p>
             <p className="font-invader text-gray-400 text-[18px] leading-none">{city}</p>
           </div>
           <p className="font-invader text-gray-300 text-[12px] leading-none">{`${timeAgo} | #${flashNumber}`}</p>
