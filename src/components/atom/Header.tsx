d1335353563e15f5693a3baf75ec5fac7c6f86c0
henry <13052648+gitpancake@users.noreply.github.com>
2025-05-06 15:35:51 -0700

    Remove neynar from Header

--- src/components/atom/Header.tsx
+++ src/components/atom/Header.tsx
@@ -1,18 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import { getSession } from "~/auth";
-import neynarClient from "~/lib/neynar/client";
+import { useFrame } from "../providers/FrameProvider";
 
-export const Header = async () => {
-  const session = await getSession();
-
-  if (!session) {
-    return null;
-  }
-
-  const {
-    users: [neynarUser],
-  } = await neynarClient.fetchBulkUsers({ fids: [session?.user.fid] });
+export const Header = () => {
+  const { context } = useFrame();
 
   return (
     <div className="flex w-full justify-between items-center bg-black px-4">
@@ -20,9 +11,11 @@ export const Header = async () => {
         <Image src="/splash.png" width={1920} height={1080} alt="Flashcastr" className="h-[60px] w-[60px]" />
       </Link>
 
-      <Link href="/profile">
-        <Image src={neynarUser.pfp_url ?? `/splash.png`} width={32} height={32} alt="Profile" />
-      </Link>
+      {context && (
+        <Link href="/profile">
+          <Image src={context?.user.pfpUrl ?? `/splash.png`} width={32} height={32} alt="Profile" />
+        </Link>
+      )}
     </div>
   );
 };
