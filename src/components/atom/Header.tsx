de32920c53c295ea7ca294eb8aa616b409ea6b92
henry <13052648+gitpancake@users.noreply.github.com>
2025-05-06 15:52:34 -0700

    Put header back on client

--- src/components/atom/Header.tsx
+++ src/components/atom/Header.tsx
@@ -1,9 +1,11 @@
-import sdk from "@farcaster/frame-sdk";
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useFrame } from "../providers/FrameProvider";
 
-export const Header = async () => {
-  const context = await sdk.context;
+export const Header = () => {
+  const { context } = useFrame();
 
   return (
     <div className="flex w-full justify-between items-center bg-black px-4">
