acb6bbc448a3fb3a5a05eb617516d7ea08f5af54
henry <13052648+gitpancake@users.noreply.github.com>
2025-05-07 14:00:42 -0700

    Introduce React.use on ProfilePage

--- src/app/profile/[fid]/page.tsx
+++ src/app/profile/[fid]/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { redirect } from "next/navigation";
+import { use } from "react";
 import { Loading } from "~/components/atom/Loading";
 import Setup from "~/components/organism/Setup";
 import UserProfile from "~/components/organism/UserProfile";
 import { useGetUser } from "~/hooks/api.flashcastrs.app/useGetUser";
 
-export default function ProfilePage({ params }: { params: { fid: string } }) {
-  const { fid } = params;
+export default function ProfilePage({ params }: { params: Promise<{ fid: string }> }) {
+  const { fid } = use(params);
 
   if (!fid || isNaN(Number(fid))) {
     redirect("/");
