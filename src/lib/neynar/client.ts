6ebe920edeabeb41abe57219e9b93ebe96f2750e
henry <13052648+gitpancake@users.noreply.github.com>
2025-09-21 07:56:27 -0700

    Remove hardcoded API key

--- src/lib/neynar/client.ts
+++ src/lib/neynar/client.ts
@@ -4,7 +4,7 @@ if (!process.env.NEYNAR_API_KEY) {
   throw new Error("NEYNAR_API_KEY is not set");
 }
 
-const config = new Configuration({ apiKey: "E70C6448-79AF-43BE-A5EE-095FCC8BC365" });
+const config = new Configuration({ apiKey: process.env.NEYNAR_API_KEY });
 
 const neynarClient = new NeynarAPIClient(config);
 
